ccae7838736b65f9133c723f366fb2f00a41eff7
Acetech0 <acem140804@gmail.com>
2025-04-08 22:33:59 +0530

    part 4 complete

--- app/src/main/java/com/example/quizapplication/Register.java
+++ app/src/main/java/com/example/quizapplication/Register.java
@@ -3,12 +3,14 @@ package com.example.quizapplication;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.QuickContactBadge;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
@@ -18,11 +20,18 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 public class Register extends AppCompatActivity {
 
@@ -117,7 +126,46 @@ public class Register extends AppCompatActivity {
 
                         if (task.isSuccessful())
                         {
-                            FirebaseUser fuser =
+                            FirebaseUser fuser = fauth.getCurrentUser();
+                            fuser.sendEmailVerification().addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void unused) {
+                                    Toast.makeText(Register.this, "Register Succesful", Toast.LENGTH_SHORT).show();
+
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Log.d(TAG,"OnFailure: Email not sent" + e.getMessage());
+
+                                }
+                            });
+
+                            Toast.makeText(Register.this, "User Created", Toast.LENGTH_SHORT).show();
+                            UserId = fauth.getCurrentUser().getUid();
+                            DocumentReference documentReference = fstore.collection("users").document(UserId);
+                            Map<String, Object> user = new HashMap<>();
+                            user.put("fname",fullname);
+                            user.put("email",email);
+                            user.put("phone",phone);
+                            documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void unused) {
+                                    Log.d(TAG, "onSuccess:user profil is created for" + UserId);
+
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Log.d(TAG,"onFaliure : "+e.toString());
+
+                                }
+                            });
+
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        }
+                        else {
+                            Toast.makeText(Register.this, "Error !"+task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                         }
 
 
