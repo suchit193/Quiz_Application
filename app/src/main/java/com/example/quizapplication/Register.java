79aea015c026d1af00293144254fda3c75c9e6fe
Acetech0 <acem140804@gmail.com>
2025-04-08 20:52:52 +0530

    3rd and 4th half done

--- app/src/main/java/com/example/quizapplication/Register.java
+++ app/src/main/java/com/example/quizapplication/Register.java
@@ -1,14 +1,43 @@
 package com.example.quizapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.QuickContactBadge;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class Register extends AppCompatActivity {
+
+    public  static final String TAG = "TAG";
+
+
+    EditText mFullname,mEmail,mPassword,mPhone;
+    Button mRegesiterBtn;
+    TextView mLoginBtn;
+    ProgressBar progressBar;
+
+    String UserId;
+
+    FirebaseAuth fauth;
+    FirebaseFirestore fstore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +48,89 @@ public class Register extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+
+        mFullname = findViewById(R.id.fullname);
+        mEmail = findViewById(R.id.Email);
+        mPassword = findViewById(R.id.password);
+        mPhone = findViewById(R.id.phone);
+        mRegesiterBtn = findViewById(R.id.registerBtn);
+        mLoginBtn = findViewById(R.id.createText);
+        progressBar = findViewById(R.id.progressbar);
+
+        fauth = FirebaseAuth.getInstance();
+        fstore = FirebaseFirestore.getInstance();
+
+        if (fauth.getCurrentUser() != null)
+        {
+            startActivity(new Intent(getApplicationContext(), Manifest.class));
+            finish();
+        }
+
+
+
+        mLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Register.this, Login.class);
+                startActivity(intent);
+            }
+        });
+
+
+        mRegesiterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                final String email = mEmail.getText().toString().trim();
+                String password = mPassword.getText().toString().trim();
+                final String fullname = mFullname.getText().toString();
+                final String phone = mPhone.getText().toString();
+
+
+                if (TextUtils.isEmpty(email)
+                {
+                    mEmail.setError("Email is Required");
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password))
+                {
+                    mEmail.setError("Password is Required");
+                    return;
+                }
+
+                if (password.length()<6)
+                {
+                    mPassword.setError("password must be >=8 characters");
+                    return;
+                }
+
+                progressBar.setVisibility(View.VISIBLE);
+
+
+
+                fauth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if (task.isSuccessful())
+                        {
+                            FirebaseUser fuser =
+                        }
+
+
+
+
+                    }
+                });
+
+
+
+
+
+            }
         });
 
 
