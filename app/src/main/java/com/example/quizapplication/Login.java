28fcc2f28e36ae66cc19a9a9bb86f56bcda6d92b
Acetech0 <acem140804@gmail.com>
2025-04-20 02:45:58 +0530

    part5 .1

--- app/src/main/java/com/example/quizapplication/Login.java
+++ app/src/main/java/com/example/quizapplication/Login.java
@@ -1,6 +1,11 @@
 package com.example.quizapplication;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +14,15 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Login extends AppCompatActivity {
+
+    EditText mEmail,mPassword;
+    Button mLoginBtn;
+    TextView mCreateBtn;
+
+    ProgressBar progressBar;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +33,29 @@ public class Login extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+
+            mEmail = findViewById(R.id.Email);
+            mPassword = findViewById(R.id.password);
+            progressBar = findViewById(R.id.progressbar);
+            mLoginBtn = findViewById(R.id.loginBtn);
+            mCreateBtn  = findViewById(R.id.createText);
+
+
+            mCreateBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                }
+            });
+
+            mLoginBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                }
+            });
+
+
         });
     }
 }
